321e2ae0eeb72fe8211b9af43afa645303cfe433
js-konda <jkonda@ucsd.edu>
2022-05-27 04:04:19 -0700

    add name cloud and format imports

--- client/nameCloud.py
+++ client/nameCloud.py
@@ -1,23 +1,29 @@
+import base64
+from io import BytesIO
+
 import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
 import plotly.graph_objects as go
 from dash.dependencies import Input, Output
+from wordcloud import WordCloud
 
 from app import app, df
 
 layout = html.Div([
-    dcc.Graph(id='top5-name-graph'),
+    dcc.Graph(id='name-cloud-graph',
+              config = {'displayModeBar':False, 'staticPlot':True}),
 
     html.Div([
             html.Div([
                 html.Div([
                     'TOP',
-                    dcc.Slider(1, 20, 1,
+                    dcc.Slider(1, 150, 10,
                                # marks=None,
-                               value=5,
-                               id='top5-name-input-th'),
+                               value=50,
+                               id='name-cloud-input-th'),
                 ],
                     style={'width': '500px',
                            # 'display': 'inline-block',
@@ -28,7 +34,7 @@ layout = html.Div([
 
                 html.Div([
                     'Year  ',
-                    dcc.Input(id='top5-name-input-year', type='number', value=2020)
+                    dcc.Input(id='name-cloud-input-year', type='number', value=2020)
                 ],
                     style={'width': '500px',
                            # 'display': 'inline-block',
@@ -40,7 +46,7 @@ layout = html.Div([
                 ),
 
                 html.Div(
-                    id='top5-name-the-alert',
+                    id='name-cloud-graph-alert',
                     children=[],
                     style={
                            # 'display': 'inline-block',
@@ -53,7 +59,7 @@ layout = html.Div([
 
                 html.Div([
                     dcc.RadioItems(
-                        id='top5-name-input-sex',
+                        id='name-cloud-input-sex',
                         options=[
                             {'label': 'Female', 'value': 'F'},
                             {'label': 'Male', 'value': 'M'},
@@ -84,11 +90,11 @@ layout = html.Div([
 alert = dbc.Alert("Please input year between 1910-2020!", color='danger', dismissable=False, duration=1500)
 
 @app.callback(
-    Output('top5-name-graph', 'figure'),
-    Output('top5-name-the-alert', 'children'),
-    Input('top5-name-input-year', 'value'),
-    Input('top5-name-input-sex', 'value'),
-    Input('top5-name-input-th', 'value')
+    Output('name-cloud-graph', 'figure'),
+    Output('name-cloud-graph-alert', 'children'),
+    Input('name-cloud-input-year', 'value'),
+    Input('name-cloud-input-sex', 'value'),
+    Input('name-cloud-input-th', 'value')
 )
 
 def update_figure(year, sex, th):
@@ -97,22 +103,23 @@ def update_figure(year, sex, th):
     year = int(year)
     th = int(th)
 
+    filtered_df = df[df['year'] == year]
+    filtered_df = filtered_df[filtered_df['sex'] == sex]
+    names = filtered_df[['name', 'count']].groupby('name').sum()
+    sort_names = names.sort_values(by='count', ascending=False)
+    sort_names_th = sort_names.head(th)
+    most_named = pd.Series(sort_names_th['count'].values, index=sort_names_th.index).to_dict()
 
-    names = df.groupby(['name', 'year', 'sex'])['count'].sum().reset_index()
-    sort_names = names.sort_values(by=['year', 'count'], ascending=False)
-    sort_names_year = sort_names[sort_names['year'] == year]
-    sort_names_year_sex = sort_names_year[sort_names_year['sex'] == sex]
-    sort_names_year_sex_th = sort_names_year_sex.head(th)
-    c = [i for i in range(th)]
-    fig = go.Figure()
-    fig.add_trace(go.Bar(x=sort_names_year_sex_th['count']
-                          , y=sort_names_year_sex_th['name']
-                          , orientation='h'
-                          , marker=dict(color=c, colorscale='RdBu')))
-    fig.update_layout(uniformtext_minsize=2, uniformtext_mode='hide', showlegend=False
-                      , yaxis={'categoryorder': 'total ascending'}
-                      , plot_bgcolor='rgba(0,128,0,0.3)'
-                      , title_text="Top " + str(th) + " Popular Names in " + str(year))
+    wc = WordCloud(background_color="white",
+                   random_state=42, width=1500, height=1500)
+    nc = wc.generate_from_frequencies(most_named)
+    nc_img = nc.to_image()
+    prefix = "data:image/png;base64,"
+    with BytesIO() as buffer:
+        nc_img.save(buffer, 'png')
+        img2 = prefix + base64.b64encode(buffer.getvalue()).decode()
+
+    fig = go.Figure(go.Image(source=img2))
     return fig, dash.no_update
 
 
